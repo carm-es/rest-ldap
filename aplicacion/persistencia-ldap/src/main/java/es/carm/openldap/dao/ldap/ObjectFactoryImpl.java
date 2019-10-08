b545aad4d1355d1cdb05ce58bc7ac1c7be84e8af
ibarrancos <ignacio.barrancos@carm.es>
2019-10-08 17:58:57 +0200

    style: Formateo del código fuente
    Se formatea el código fuente del proyecto, usando la plantilla
    de Google
    Issue: #1

--- aplicacion/persistencia-ldap/src/main/java/es/carm/openldap/dao/ldap/ObjectFactoryImpl.java
+++ aplicacion/persistencia-ldap/src/main/java/es/carm/openldap/dao/ldap/ObjectFactoryImpl.java
@@ -20,159 +20,157 @@ import es.carm.openldap.dac.UsuarioDAC;
 import es.carm.openldap.dao.FactoryDao;
 import es.carm.openldap.dao.ldap.impl.UserFactoryDAOImpl;
 
-
 public class ObjectFactoryImpl implements es.carm.openldap.dao.ObjectFactory, Serializable {
-	
-	static Logger log = LogManager.getLogger( ObjectFactoryImpl.class );	
-	
-	private static final long serialVersionUID = -8380017861016136801L;
 
-	private static final String CONFIG_DEFAULT_FILE="ldap-config.properties";
-	
-	private BlockingConnectionPool pool =null;
-	
-	private String config_Ldap_URL=null;
-	private String config_Bind_DN=null;
-	private String config_Bind_Password=null;
-	private String config_BaseDN=null;
-	private String config_FilterSearchUsers=null;
-	private boolean useSSL=false;
-	private long connectionTimeout=10000;
-	
+    static Logger log = LogManager.getLogger(ObjectFactoryImpl.class);
 
-	public String toString() {
-		String retVal="";
-		
-		retVal +="[LDAP-CONECTION init='"+ (pool!=null) +"']\n";
-		retVal +="  config.ldap.url="+ config_Ldap_URL +"\n";
-		retVal +="  config.ldap.bind.user="+ config_Bind_DN +"\n";
-		retVal +="  config.ldap.bind.password="+ config_Bind_Password +"\n";
-		retVal +="  config.ldap.base="+ config_BaseDN +"\n";
-		retVal +="  config.ldap.filter.search.users="+ config_FilterSearchUsers +"\n";
-		retVal +="  config.ldap.connection.timeout="+ connectionTimeout +"\n";
-		retVal +="  config.ldap.connection.use_ssl="+ useSSL +"\n";
-				
-		return retVal;
-	}
-	
-	private void initialize() {
-		if ( null!=pool ) {
-			try {
-				pool.close();
-				pool=null;
-			}catch(Exception x) {
-			}
-		}
-		BindConnectionInitializer bind=new BindConnectionInitializer();
-		bind.setBindDn( config_Bind_DN );
-		bind.setBindCredential( new Credential(config_Bind_Password) );
-		
-		ConnectionConfig cc=new ConnectionConfig();
-		cc.setLdapUrl( config_Ldap_URL );
-		cc.setConnectionInitializer( bind );
-		cc.setUseSSL(useSSL);
-		cc.setConnectTimeout( connectionTimeout );
-		
-		DefaultConnectionFactory connectionFactory = new DefaultConnectionFactory();
-		connectionFactory.setConnectionConfig( cc );
-		
-		pool = new BlockingConnectionPool( connectionFactory );
-		pool.initialize();
-	}
-	
-	private boolean configurate(Properties prop) {
-		config_Ldap_URL=prop.getProperty("config.ldap.url");
-		config_Bind_DN=prop.getProperty("config.ldap.bind.user");
-		config_Bind_Password=prop.getProperty("config.ldap.bind.password");
-		config_BaseDN=prop.getProperty("config.ldap.base");
-		config_FilterSearchUsers=prop.getProperty("config.ldap.filter.search.users");	
-		try {
-			connectionTimeout=Long.parseLong( prop.getProperty("config.ldap.connection.timeout") );
-		}catch(Exception x) {
-		}
+    private static final long serialVersionUID = -8380017861016136801L;
 
-		if ( ( "yes".equalsIgnoreCase( prop.getProperty("config.ldap.connection.use_ssl") )) || ( "si".equalsIgnoreCase( prop.getProperty("config.ldap.connection.use_ssl") )) ) {
-			useSSL=true;
-		}
-		
-		return (null!=config_Ldap_URL) && (null!=config_Bind_DN) && (null!=config_Bind_Password) && (null!=config_BaseDN);
-	}
-	
-	public ObjectFactoryImpl(Properties prop) {		
-		if ( this.configurate(prop) ) {	
-			this.initialize();
-		}
-	}
-	
-	public ObjectFactoryImpl(String ldapUrl, String dnLogin, String password, String baseLDAP) {		
-		config_Ldap_URL=ldapUrl;
-		config_Bind_DN=dnLogin;
-		config_Bind_Password=password;
-		config_BaseDN=baseLDAP;
-		
-		this.initialize();
-	}
+    private static final String CONFIG_DEFAULT_FILE = "ldap-config.properties";
 
-	public ObjectFactoryImpl() {	
-		boolean init=false;
-		Properties properties = new Properties();
-		try {
-			InputStream stream = this.getClass().getResourceAsStream("/" + CONFIG_DEFAULT_FILE);
-			if (null==stream) {
-				stream = this.getClass().getClassLoader().getResourceAsStream(CONFIG_DEFAULT_FILE);	
-			}
-			if (null!= stream ) {
-				properties.load(stream);
-				init=this.configurate( properties );
-			} else {
-				log.error("ERROR: ObjectFactoryImpl sin configurar!!!!" );				
-			}
-			
-		}catch(Exception x) {
-			log.error("Al inicializar la factoria", x );
-		}
-		if (init) {
-			this.initialize();
-		}
-	}
+    private BlockingConnectionPool pool = null;
 
-	
+    private String config_Ldap_URL = null;
+    private String config_Bind_DN = null;
+    private String config_Bind_Password = null;
+    private String config_BaseDN = null;
+    private String config_FilterSearchUsers = null;
+    private boolean useSSL = false;
+    private long connectionTimeout = 10000;
 
-	public FactoryDao<UsuarioDAC> getUsuariosDAO() {
-		return new UserFactoryDAOImpl( new PooledConnectionFactory(pool) , config_BaseDN, config_FilterSearchUsers );
-	}
+    public String toString() {
+        String retVal = "";
 
-	public void configure(String fileProperties) {
-		boolean init=false;
-		Properties properties = new Properties();
-		try {
-			File config=new File(fileProperties);
-			if ( config.exists() ) {
-				InputStream stream=null;
-				try {
-					stream =  new FileInputStream( config );
-					properties.load(stream);
-					init=this.configurate( properties );
-					
-				}catch(Exception x) {
-					log.error("Al configurar la factoria", x );
-				}finally {
-					if ( null != stream ) {
-						stream.close();
-					}
-				}
-			} else {
-				log.error("No se pudo configurar la aplicación desde el fichero '"+ fileProperties +"'");
-			}
-			
-		}catch(Exception x) {
-			log.error("Al leer la configuración de la factoria", x );
-		}
-		if (init) {
-			log.info("La aplicación se configuró desde el fichero '"+ fileProperties +"'");
-			this.initialize();
-		}
-		
-	}
+        retVal += "[LDAP-CONECTION init='" + (pool != null) + "']\n";
+        retVal += "  config.ldap.url=" + config_Ldap_URL + "\n";
+        retVal += "  config.ldap.bind.user=" + config_Bind_DN + "\n";
+        retVal += "  config.ldap.bind.password=" + config_Bind_Password + "\n";
+        retVal += "  config.ldap.base=" + config_BaseDN + "\n";
+        retVal += "  config.ldap.filter.search.users=" + config_FilterSearchUsers + "\n";
+        retVal += "  config.ldap.connection.timeout=" + connectionTimeout + "\n";
+        retVal += "  config.ldap.connection.use_ssl=" + useSSL + "\n";
+
+        return retVal;
+    }
+
+    private void initialize() {
+        if (null != pool) {
+            try {
+                pool.close();
+                pool = null;
+            } catch (Exception x) {
+            }
+        }
+        BindConnectionInitializer bind = new BindConnectionInitializer();
+        bind.setBindDn(config_Bind_DN);
+        bind.setBindCredential(new Credential(config_Bind_Password));
+
+        ConnectionConfig cc = new ConnectionConfig();
+        cc.setLdapUrl(config_Ldap_URL);
+        cc.setConnectionInitializer(bind);
+        cc.setUseSSL(useSSL);
+        cc.setConnectTimeout(connectionTimeout);
+
+        DefaultConnectionFactory connectionFactory = new DefaultConnectionFactory();
+        connectionFactory.setConnectionConfig(cc);
+
+        pool = new BlockingConnectionPool(connectionFactory);
+        pool.initialize();
+    }
+
+    private boolean configurate(Properties prop) {
+        config_Ldap_URL = prop.getProperty("config.ldap.url");
+        config_Bind_DN = prop.getProperty("config.ldap.bind.user");
+        config_Bind_Password = prop.getProperty("config.ldap.bind.password");
+        config_BaseDN = prop.getProperty("config.ldap.base");
+        config_FilterSearchUsers = prop.getProperty("config.ldap.filter.search.users");
+        try {
+            connectionTimeout = Long.parseLong(prop.getProperty("config.ldap.connection.timeout"));
+        } catch (Exception x) {
+        }
+
+        if (("yes".equalsIgnoreCase(prop.getProperty("config.ldap.connection.use_ssl")))
+                || ("si".equalsIgnoreCase(prop.getProperty("config.ldap.connection.use_ssl")))) {
+            useSSL = true;
+        }
+
+        return (null != config_Ldap_URL) && (null != config_Bind_DN) && (null != config_Bind_Password)
+                && (null != config_BaseDN);
+    }
+
+    public ObjectFactoryImpl(Properties prop) {
+        if (this.configurate(prop)) {
+            this.initialize();
+        }
+    }
+
+    public ObjectFactoryImpl(String ldapUrl, String dnLogin, String password, String baseLDAP) {
+        config_Ldap_URL = ldapUrl;
+        config_Bind_DN = dnLogin;
+        config_Bind_Password = password;
+        config_BaseDN = baseLDAP;
+
+        this.initialize();
+    }
+
+    public ObjectFactoryImpl() {
+        boolean init = false;
+        Properties properties = new Properties();
+        try {
+            InputStream stream = this.getClass().getResourceAsStream("/" + CONFIG_DEFAULT_FILE);
+            if (null == stream) {
+                stream = this.getClass().getClassLoader().getResourceAsStream(CONFIG_DEFAULT_FILE);
+            }
+            if (null != stream) {
+                properties.load(stream);
+                init = this.configurate(properties);
+            } else {
+                log.error("ERROR: ObjectFactoryImpl sin configurar!!!!");
+            }
+
+        } catch (Exception x) {
+            log.error("Al inicializar la factoria", x);
+        }
+        if (init) {
+            this.initialize();
+        }
+    }
+
+    public FactoryDao<UsuarioDAC> getUsuariosDAO() {
+        return new UserFactoryDAOImpl(new PooledConnectionFactory(pool), config_BaseDN, config_FilterSearchUsers);
+    }
+
+    public void configure(String fileProperties) {
+        boolean init = false;
+        Properties properties = new Properties();
+        try {
+            File config = new File(fileProperties);
+            if (config.exists()) {
+                InputStream stream = null;
+                try {
+                    stream = new FileInputStream(config);
+                    properties.load(stream);
+                    init = this.configurate(properties);
+
+                } catch (Exception x) {
+                    log.error("Al configurar la factoria", x);
+                } finally {
+                    if (null != stream) {
+                        stream.close();
+                    }
+                }
+            } else {
+                log.error("No se pudo configurar la aplicación desde el fichero '" + fileProperties + "'");
+            }
+
+        } catch (Exception x) {
+            log.error("Al leer la configuración de la factoria", x);
+        }
+        if (init) {
+            log.info("La aplicación se configuró desde el fichero '" + fileProperties + "'");
+            this.initialize();
+        }
+
+    }
 
 }
--- aplicacion/persistencia-ldap/src/main/java/es/carm/openldap/dao/ldap/impl/UserFactoryDAOImpl.java
+++ aplicacion/persistencia-ldap/src/main/java/es/carm/openldap/dao/ldap/impl/UserFactoryDAOImpl.java
@@ -1,7 +1,5 @@
 package es.carm.openldap.dao.ldap.impl;
 
-
-
 import org.ldaptive.Connection;
 import org.ldaptive.LdapEntry;
 import org.ldaptive.LdapException;
@@ -18,78 +16,79 @@ import es.carm.openldap.NoDataFound;
 import es.carm.openldap.dac.UsuarioDAC;
 import es.carm.openldap.dao.FactoryDao;
 
-public class UserFactoryDAOImpl implements FactoryDao<UsuarioDAC>{
+public class UserFactoryDAOImpl implements FactoryDao<UsuarioDAC> {
 
-	private PooledConnectionFactory connFactory = null;
-	private String baseDN=null;
-	private String filter="(&(objectClass=CARMfuncionario)(objectClass=inetOrgPerson)(|(cn=%%ID%%)(DNI=%%ID%%)(uid=%%ID%%)))";
-	
-	protected UserFactoryDAOImpl(String baseLDAP) {	
-		baseDN=baseLDAP;
-	}
-	
-	public UserFactoryDAOImpl(PooledConnectionFactory conFactory, String baseLDAP, String filterLDAP ) {
-		connFactory=conFactory;
-		baseDN=baseLDAP;
-		if ( (null!=filterLDAP) && (0 < filterLDAP.length()) ) {
-			filter=filterLDAP;
-		}
-	}
+    private PooledConnectionFactory connFactory = null;
+    private String baseDN = null;
+    private String filter = "(&(objectClass=CARMfuncionario)(objectClass=inetOrgPerson)(|(cn=%%ID%%)(DNI=%%ID%%)(uid=%%ID%%)))";
 
-	private String resolveFilter(String id) {
-		String retVal=filter;
-		if ( (null!= filter) && (0<filter.length()) ) {
-			retVal=filter.replaceAll("%%ID%%", id);
-		}
-		return retVal;
-	}
-	
-	private UsuarioDAC getByFilter(Connection conn, String filter) throws ConnectionError{
-		UsuarioDAC retVal=null;
-		try {
-			SearchOperation search = new SearchOperation(conn);
-			SearchRequest request = new SearchRequest(baseDN, filter );
-			request.setReturnAttributes( ReturnAttributes.ALL.value());
-			request.setSearchEntryHandlers(new ObjectSidHandler(), new ObjectGuidHandler());
-			
-			SearchResult result = search.execute(request).getResult();
-			for (LdapEntry entry : result.getEntries()) {		
-				retVal=  es.carm.openldap.dac.dao.entryldap.UserFactory.fromEntry( entry );							
-			}
-			
-		} catch (LdapException e) {
-			throw new ConnectionError(e);				
-		}
-		return retVal;
-	}
+    protected UserFactoryDAOImpl(String baseLDAP) {
+        baseDN = baseLDAP;
+    }
 
-	public UsuarioDAC get(String id) throws NoDataFound,ConnectionError {
-		UsuarioDAC retVal=null;
-		
-		Connection conn = null;
-		try {
-			conn = connFactory.getConnection();
-		} catch (LdapException e) {
-			throw new ConnectionError(e);
-		}
-		if ( null != conn ) {
-			try {
-				conn.open();
-			} catch (LdapException e) {
-				throw new ConnectionError(e);
-			}			 
-			try {
-				retVal=this.getByFilter(conn, resolveFilter(id)  );				
-			} finally {
-				try { conn.close(); } catch(Exception x) { }				
-			}
-		}
-		if ( null == retVal ) {
-			throw new NoDataFound();
-		}
-		return retVal;		
-	}
-	
-	
+    public UserFactoryDAOImpl(PooledConnectionFactory conFactory, String baseLDAP, String filterLDAP) {
+        connFactory = conFactory;
+        baseDN = baseLDAP;
+        if ((null != filterLDAP) && (0 < filterLDAP.length())) {
+            filter = filterLDAP;
+        }
+    }
+
+    private String resolveFilter(String id) {
+        String retVal = filter;
+        if ((null != filter) && (0 < filter.length())) {
+            retVal = filter.replaceAll("%%ID%%", id);
+        }
+        return retVal;
+    }
+
+    private UsuarioDAC getByFilter(Connection conn, String filter) throws ConnectionError {
+        UsuarioDAC retVal = null;
+        try {
+            SearchOperation search = new SearchOperation(conn);
+            SearchRequest request = new SearchRequest(baseDN, filter);
+            request.setReturnAttributes(ReturnAttributes.ALL.value());
+            request.setSearchEntryHandlers(new ObjectSidHandler(), new ObjectGuidHandler());
+
+            SearchResult result = search.execute(request).getResult();
+            for (LdapEntry entry : result.getEntries()) {
+                retVal = es.carm.openldap.dac.dao.entryldap.UserFactory.fromEntry(entry);
+            }
+
+        } catch (LdapException e) {
+            throw new ConnectionError(e);
+        }
+        return retVal;
+    }
+
+    public UsuarioDAC get(String id) throws NoDataFound, ConnectionError {
+        UsuarioDAC retVal = null;
+
+        Connection conn = null;
+        try {
+            conn = connFactory.getConnection();
+        } catch (LdapException e) {
+            throw new ConnectionError(e);
+        }
+        if (null != conn) {
+            try {
+                conn.open();
+            } catch (LdapException e) {
+                throw new ConnectionError(e);
+            }
+            try {
+                retVal = this.getByFilter(conn, resolveFilter(id));
+            } finally {
+                try {
+                    conn.close();
+                } catch (Exception x) {
+                }
+            }
+        }
+        if (null == retVal) {
+            throw new NoDataFound();
+        }
+        return retVal;
+    }
 
 }
